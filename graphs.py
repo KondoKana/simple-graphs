130617e0cd9f07a878020d968d53d8337a91200c
Teegeetiger <peteralandawson@gmail.com>
2026-05-14 18:12:51 -0400

    bindings works, vertecies work. TODO horizonal and vertical centering

--- graphs.py
+++ graphs.py
@@ -17,34 +17,199 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 colors = pygame.colordict.THECOLORS
+bindings = []
+used_mods = set()
 
-class options:
+
+class State:
+    def __init__(self):
+        self.creating_edge = False
+        self.edge_start = None
+        self.can_input = True
+state = State()
+
+
+#im having way more fun with shit like this but its not the project. Still cool organization
+class Options:
     def __init__(self,
-                  show_hitboxes: Literal["Full", "Small", "None"] = "None",
-                  show_mouse_pos = True):
+                  show_hitboxes: Literal["Full", "Small", "None"] = "None"):
         self.show_hitboxes = show_hitboxes
-        self.show_mouse_pos = show_mouse_pos
-options = options()
-class binding:
+        self.show_mouse_pos = True
+        self.show_hitbox_on_hover = True
+        self.rainbow_boxes = False
+        self.grid_width = 1
+        self.ghost_width = 3
+        self.vertex_color = pygame.colordict.THECOLORS["black"]
+        self.vertex_width = 0   #0 means filled
+        self.ghost_color = pygame.colordict.THECOLORS["red"]
+        self.ghost_width = 0
+        self.edge_color = pygame.colordict.THECOLORS["black"]
+        self.edge_width = 1
+        self.min_dm = 4
+options = Options()
+
+"""
+This is me having fun with bindings and stuff. Unfortunately doing bitwise & with no mod doesnt work when things like capslock are pressed. This has a function
+to handle that - get_mods - which returns only the mods I care about from the currently active ones. 
+"""
+class Binding:
     def __init__(self, event, button=None, key=None, mod=pygame.KMOD_NONE):
         self.event = event
         self.button = button
         self.key = key
         self.mod = mod
 
-class command:
-    def __init__(self, name, bindings: list[binding], func = None):
+class Command:
+    def __init__(self, name, bindings: list[Binding], func = None):
         self.name = name
         self.func = func
         self.bindings = bindings
 
+
+    def get_mods(self):
+        mods = pygame.key.get_mods()
+        current_used = pygame.KMOD_NONE
+        for mod in used_mods:
+            if mods & mod:
+                current_used |= mod
+        return current_used
+        
     def evoke(self, event: pygame.event.Event):
+        mods = self.get_mods()
         for binding in self.bindings:
-            if event.type == binding.event and (getattr(event, "key", None) is None or event.key == binding.key) and ((binding.mod == pygame.KMOD_NONE) ^ (pygame.key.get_mods() & binding.mod)) and (getattr(event, "button", None) is None or event.button == binding.button):
+            #check key, mod, and button. Seeing as how mouse is based on button probably could do some shortcutting but eeh
+            if (event.type == binding.event and                                                                 #correct event type
+                (binding.key is  None or event.key == binding.key) and                                          #correct key if applicable
+                ((binding.mod & mods) or (binding.mod == pygame.KMOD_NONE and mods == pygame.KMOD_NONE)) and    #correct mod or nomod if none are pressed
+                (getattr(event, "button", None) is None or event.button == binding.button)):                    #correct button if applicable
                 if self.func is not None:
-                    self.func()
+                    self.func(event)
                 return True
         return False
+
+"""
+Bindings:
+create_vertex
+create_ghost_vertex
+add_edge
+delete_vertex
+delete_edge
+"""
+
+def create_bindings():
+    create_vertex  = Command("create_vertex",[
+        Binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_LEFT, mod=pygame.KMOD_NONE),
+        Binding(pygame.KEYDOWN, key =pygame.K_q, mod=pygame.KMOD_NONE)], 
+        func= create_vertex_func)
+    
+    create_ghost_vertex = Command("create_ghost_vertex",[
+        Binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_MIDDLE, mod=pygame.KMOD_NONE),
+        Binding(pygame.KEYDOWN, key=pygame.K_w)],
+        func=create_ghost_vertex_func)
+    
+    delete_vertex = Command("delete_vertex",[
+        Binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_LEFT, mod=pygame.KMOD_SHIFT),
+        Binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_MIDDLE, mod=pygame.KMOD_SHIFT),
+        Binding(pygame.KEYDOWN, key=pygame.K_q, mod=pygame.KMOD_SHIFT),
+        Binding(pygame.KEYDOWN, key=pygame.K_w, mod=pygame.KMOD_SHIFT)], 
+        func=delete_vertex_func)
+    
+    add_edge = Command("add_edge",[
+        Binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_RIGHT, mod=pygame.KMOD_NONE),
+        Binding(pygame.KEYDOWN, key=pygame.K_e, mod=pygame.KMOD_NONE)], 
+        func=create_edge_func)
+    
+    cancel_edge = Command("cancel_edge",[
+        Binding(pygame.KEYDOWN, key=pygame.K_ESCAPE, mod=pygame.KMOD_NONE)], 
+        func=cancel_edge_func)
+    
+    delete_edge = Command("delete_edge",[
+        Binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_RIGHT, mod=pygame.KMOD_SHIFT),
+        Binding(pygame.KEYDOWN, key=pygame.K_e, mod=pygame.KMOD_SHIFT)], 
+        func=delete_edge_func)
+    
+
+    #any_key = command("any_key", [binding(pygame.KEYDOWN)], func=check_mods())
+    bindings = [create_vertex, create_ghost_vertex, add_edge, cancel_edge, delete_vertex, delete_edge]
+    used_mods = set()
+    print(bindings)
+    for command in bindings:
+        for binding in command.bindings:
+            if binding.mod is not None and binding.mod != pygame.KMOD_NONE:
+                used_mods.add(binding.mod)
+    print(bindings)
+    print(used_mods)
+    print(pygame.KMOD_SHIFT)
+    return bindings,used_mods
+
+def check_bindings(event):
+    for command in bindings:
+        if command.evoke(event):
+            continue
+
+
+def create_vertex_func(event=None, ghost = False):
+    if not state.can_input:
+        return
+    state.can_input = False
+    mouse_grid_pos = get_mouse_grid_pos(gr)
+    if mouse_grid_pos != (None, None):
+        (x,x_t), (y,y_t) = mouse_grid_pos
+        if x_t != hitbox_type.VERTEX:
+            gr.grid.add_row(-1)
+            gr.grid.add_row(gr.grid.height-1)
+            gr.grid.add_col(x)
+            if x >= (gr.grid.width-2)/2:
+                gr.grid.add_col(-1)
+                x+=1
+            else:
+                gr.grid.add_col(gr.grid.width-1)
+            
+            gr.update_grid(gr.grid)
+            x+=1
+            y+=1
+        if y_t != hitbox_type.VERTEX:
+            gr.grid.add_col(-1)
+            gr.grid.add_col(gr.grid.width-1)
+            gr.grid.add_row(y)
+            if y >= (gr.grid.height-2)/2:
+                gr.grid.add_row(-1)
+                y+=1
+            else:
+                gr.grid.add_row(gr.grid.height-1)
+            
+            y+=1
+            x+=1
+            gr.update_grid(gr.grid)
+
+        if gr.grid.get_vertex(x,y) is not None and not gr.grid.get_vertex(x,y).ghost:
+            print("vertex already exists at", mouse_grid_pos)
+            return
+        print(f"create vertex at {mouse_grid_pos}")
+        if ghost:
+            print("ghost vertex")
+        gr.grid.add_vertex(x,y,ghost=ghost, 
+                              color=options.ghost_color if ghost else options.vertex_color,
+                              width=options.ghost_width if ghost else options.vertex_width)
+        gr.grid.trim(options.min_dm)
+        gr.update_grid(gr.grid)
+            
+
+def create_ghost_vertex_func(event=None):
+    create_vertex_func(event, ghost=True)
+
+def create_edge_func(event=None):
+    print("add edge")
+
+def delete_vertex_func(event=None):
+    print("delete vertex")
+
+def cancel_edge_func(event=None):
+    print("cancel edge")
+
+def delete_edge_func(event=None):
+    print("delete edge")
+
 
 class hitbox_type:
     VERTEX = "vertex"
@@ -64,8 +229,10 @@ class hitbox:
 # actual display grid which holds the grid backend and also has some display related properties
 class d_grid:
     def __init__ (self, grid: grid.Grid, line_width = 1):
+        self.grid = grid
         self.update_grid(grid)
         self.line_width = line_width
+    
 
     def update_grid(self, grid: grid.Grid):
         self.grid_w = grid.width
@@ -80,50 +247,49 @@ class d_grid:
         color_iter = iter(color_list)
         #creates top large hitbox
         rect = pygame.Rect((0, 0), (self.grid_size_px, self.line_spacing*1.25))
-        self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.HORIZONTAL, value=-1, color=colors[next(color_iter)]))
-        #creates a left large hitbox
+        self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.HORIZONTAL, value=-1, color=(colors[next(color_iter)] if options.rainbow_boxes else "grey")))
+        #creates left large hitbox
         rect = pygame.Rect((0, 0), (self.line_spacing*1.25, self.grid_size_px))
-        self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.VERTICAL, value=-1, color=colors[next(color_iter)]))
+        self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.VERTICAL, value=-1, color=(colors[next(color_iter)] if options.rainbow_boxes else "grey")))
 
 
         for i in range((self.num_cr*2)-1):
             #makes the vertical hitboxes for the vertices and edges, alternating between the two types and coloring them differently for testing purposes
             rect = pygame.Rect((self.line_spacing*1.25 + i*self.line_spacing*1/2, 0), (self.line_spacing*1/2, self.grid_size_px))
             if i%2 == 0:
-                self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.VERTICAL, value=int(i/2), color=colors[next(color_iter)]))
+                self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.VERTICAL, value=int(i/2), color=(colors[next(color_iter)] if options.rainbow_boxes else "grey")))
             else:
-                self.hitboxes.append(hitbox(rect, hitbox_type.EDGE, orientation.VERTICAL, value=int(i/2), color=colors[next(color_iter)]))
+                self.hitboxes.append(hitbox(rect, hitbox_type.EDGE, orientation.VERTICAL, value=int(i/2), color=(colors[next(color_iter)] if options.rainbow_boxes else "grey")))
             
             #makes the horizontal hitboxes for the vertices and edges, alternating between the two types and coloring them differently for testing purposes
             rect = pygame.Rect((0, self.line_spacing*1.25 + i*self.line_spacing*1/2), (self.grid_size_px, self.line_spacing*1/2))
             if i%2 == 0:
-                self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.HORIZONTAL, value=int(i/2), color=colors[next(color_iter)]))
+                self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.HORIZONTAL, value=int(i/2), color=(colors[next(color_iter)] if options.rainbow_boxes else "grey")))
             else:
-                self.hitboxes.append(hitbox(rect, hitbox_type.EDGE, orientation.HORIZONTAL, value=int(i/2), color=colors[next(color_iter)]))
+                self.hitboxes.append(hitbox(rect, hitbox_type.EDGE, orientation.HORIZONTAL, value=int(i/2), color=(colors[next(color_iter)] if options.rainbow_boxes else "grey")))
         
         #creates a right large hitbox
         rect = pygame.Rect((self.grid_size_px - self.line_spacing*1.25, 0), (self.line_spacing*1.25, self.grid_size_px))
-        self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.VERTICAL, value=self.num_cr, color=colors[next(color_iter)]))
+        self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.VERTICAL, value=self.num_cr, color=(colors[next(color_iter)] if options.rainbow_boxes else "grey")))
         #creates a bottom large hitbox
         rect = pygame.Rect((0, self.grid_size_px - self.line_spacing*1.25), (self.grid_size_px, self.line_spacing*1.25))
-        self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.HORIZONTAL, value=self.num_cr, color=colors[next(color_iter)]))
+        self.hitboxes.append(hitbox(rect, hitbox_type.VERTEX, orientation.HORIZONTAL, value=self.num_cr, color=(colors[next(color_iter)] if options.rainbow_boxes else "grey")))
 
 gr = d_grid(grid.Grid(3,3))
 
 def display_grid(grid: d_grid):
-    curernt_mouse = (None, None)
     pos = pygame.mouse.get_pos()
     screen.fill("white")
     for hitbox in grid.hitboxes:
         if hitbox.orientation == orientation.HORIZONTAL:
             #various options for displaying the hitboxes for testing purposes
-            if options.show_hitboxes == "Full":
+            if options.show_hitboxes == "Full" or (options.show_hitbox_on_hover and hitbox.rect.collidepoint(pos)):
                 pygame.draw.rect(screen, hitbox.color, hitbox.rect)
             elif options.show_hitboxes == "Small":
                 pygame.draw.rect(screen, hitbox.color, hitbox.rect.scale_by(1,0.5))
         else:
             #various options for displaying the hitboxes for testing purposes
-            if options.show_hitboxes == "Full":
+            if options.show_hitboxes == "Full" or (options.show_hitbox_on_hover and hitbox.rect.collidepoint(pos)):
                 pygame.draw.rect(screen, hitbox.color, hitbox.rect)
             elif options.show_hitboxes == "Small":
                 pygame.draw.rect(screen, hitbox.color, hitbox.rect.scale_by(0.5,1))
@@ -131,74 +297,67 @@ def display_grid(grid: d_grid):
     for i in range(grid.num_lines):
         pygame.draw.line(screen, "black", (grid.line_spacing*(i+0.5), 0), (grid.line_spacing*(i+0.5), grid.grid_size_px))
         pygame.draw.line(screen, "black", (0, grid.line_spacing*(i+0.5)), (grid.grid_size_px, grid.line_spacing*(i+0.5)))
-    if options.show_mouse_pos == True and curernt_mouse != (None, None):
+    
+
+    for vertex in grid.grid.get_all_vertices():
+        if vertex is not None:
+            x, y = vertex.x, vertex.y
+            pygame.draw.circle(screen, color=vertex.color, width=vertex.width, center=(grid.line_spacing*(x+1.5), grid.line_spacing*(y+1.5)), radius=grid.line_spacing/4)
+
+
+    if options.show_mouse_pos == True: 
         font = pygame.font.SysFont(None, 24)
-        img = font.render(str(curernt_mouse), True, "black")
+        img = font.render(str(get_mouse_grid_pos(grid)), True, "black")
         screen.blit(img, pos)
 
+
 def get_mouse_grid_pos(grid: d_grid):
+    """Get the grid position the mouse is currently hovering over.
+    
+    Args:
+        grid: d_grid - the display grid object
+        
+    Returns:
+        (x,y) coordinates of the grid position the mouse is currently hovering over, 
+        or (None, None) if the mouse is not hovering over any grid position. 
+        The coordinates are based on the hitboxes defined in the d_grid class.
+    """
     pos = pygame.mouse.get_pos()
     curernt_mouse = (None, None)
     for hitbox in grid.hitboxes:
         if hitbox.rect.collidepoint(pos):
             if hitbox.orientation == orientation.HORIZONTAL:
-                curernt_mouse = (curernt_mouse[0], hitbox.value)
+                curernt_mouse = (curernt_mouse[0], (hitbox.value,hitbox.type))
             else:
-                curernt_mouse = (hitbox.value, curernt_mouse[1])
-    return curernt_mouse
+                curernt_mouse = ((hitbox.value,hitbox.type), curernt_mouse[1])
+    return (None, None) if None in curernt_mouse else curernt_mouse
 
-"""
-Bindings:
-create_vertex
-create_ghost_vertex
-add_edge
-delete_vertex
-delete_edge
-"""
-create_vertex = command("create_vertex", 
-                        [binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_LEFT, mod=pygame.KMOD_NONE),
-                         binding(pygame.KEYDOWN, pygame.K_q)], 
-                         func=lambda: print("create vertex"))
-create_ghost_vertex = command("create_ghost_vertex",
-                             [binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_MIDDLE, mod=pygame.KMOD_NONE),
-                              binding(pygame.KEYDOWN, pygame.K_w)],
-                             func=lambda: print("create ghost vertex"))
-delete_vertex = command("delete_vertex",
-                        [binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_LEFT, mod=pygame.KMOD_SHIFT),
-                         binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_MIDDLE, mod=pygame.KMOD_SHIFT),
-                         binding(pygame.KEYDOWN, pygame.K_q, mod=pygame.KMOD_SHIFT),
-                         binding(pygame.KEYDOWN, pygame.K_w, mod=pygame.KMOD_SHIFT)], 
-                         func=lambda: print("delete vertex"))
-add_edge = command("add_edge", 
-                   [binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_RIGHT, mod=pygame.KMOD_NONE),
-                    binding(pygame.KEYDOWN, pygame.K_e)], 
-                    func=lambda: print("add edge"))
-cancel_edge = command("cancel_edge", 
-                      [binding(pygame.KEYDOWN, pygame.K_ESCAPE)], 
-                      func=lambda: print("cancel edge"))
 
-delete_edge = command("delete_edge",
-                      [binding(pygame.MOUSEBUTTONDOWN, button=pygame.BUTTON_RIGHT, mod=pygame.KMOD_SHIFT),
-                       binding(pygame.KEYDOWN, pygame.K_e, mod=pygame.KMOD_SHIFT)], 
-                       func=lambda: print("delete edge"))
-bindings = [create_vertex, create_ghost_vertex, add_edge, cancel_edge, delete_vertex, delete_edge]
+def check_mods():
+    mods = pygame.key.get_mods()
+    current_used = 0
+    for mod in used_mods:
+        if mods & mod:
+            current_used |= mod
+    pygame.key.set_mods(current_used)
 
-def check_bindings(event):
-    for command in bindings:
-        if command.evoke(event):
-            continue
 
+
+pygame.event.set_blocked(pygame.MOUSEMOTION)
+bindings, used_mods = create_bindings()
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     dt = clock.tick(60) / 1000.0
+    state.can_input = True
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         check_bindings(event)
         print(event)
         manager.process_events(event)
-
+    
+    
     display_grid(gr)
     manager.update(dt)
     manager.draw_ui(screen)
--- grid.py
+++ grid.py
@@ -15,47 +15,124 @@ class Grid:
         self.grid = np.full((height, width), None)
         self.edges = []
 
+
+    def expand(self):
+        """Expand the grid by adding a new row and column on each side."""
+        new_col = np.full((self.height, 1), None)
+        self.grid = np.append(new_col,self.grid, axis=1)
+        self.grid = np.append(self.grid, new_col, axis=1)
+        self.width += 2
+        new_row = np.full((1, self.width), None)
+        self.grid = np.append(new_row,self.grid, axis=0)
+        self.grid = np.append(self.grid,new_row, axis=0)
+        self.height += 2
+    
+    def trim(self,min_dim):
+        self.trim_tl(min_dim)
+        self.trim_br(min_dim)
+        self.trim_tr(min_dim)
+        self.trim_bl(min_dim)
+    
+    def trim_tl(self,min_dim):
+        min_tl = max(self.width, self.height)
+        for x in range(self.width):
+            if any(isinstance(self.grid[y][x], Vertex) for y in range(self.height)):
+                min_tl = min(min_tl, x)
+                print(self.grid)
+                break
+        for y in range(self.height):
+            if any(isinstance(self.grid[y][x], Vertex) for x in range(self.width)):
+                min_tl = min(min_tl, y)
+                break
+        print("min_tl:", min_tl)
+        for _ in range(min_tl):
+            if self.height >= min_dim and self.width >= min_dim:
+                self.grid = self.grid[1:, 1:]
+                self.height -= 1
+                self.width -= 1
+    
+    def trim_br(self,min_dim):
+        min_br = max(self.width, self.height)
+        for x in range(self.width-1, -1, -1):
+            if any(isinstance(self.grid[y][x], Vertex) for y in range(self.height)):
+                min_br = min(min_br, self.width - 1 - x)
+                break
+        for y in range(self.height-1, -1, -1):
+            if any(isinstance(self.grid[y][x], Vertex) for x in range(self.width)):
+                min_br = min(min_br, self.height - 1 - y)
+                break
+        print("min_br:", min_br)
+        for _ in range(min_br):
+            if self.height >= min_dim and self.width >= min_dim:
+                self.grid = self.grid[:-1, :-1]
+                self.height -= 1
+                self.width -= 1
+    
+    def trim_tr(self,min_dim):
+        min_tr = max(self.width, self.height)
+        for x in range(self.width-1, -1, -1):
+            if any(isinstance(self.grid[y][x], Vertex) for y in range(self.height)):
+                min_tr = min(min_tr, self.width - 1 - x)
+                break
+        for y in range(self.height):
+            if any(isinstance(self.grid[y][x], Vertex) for x in range(self.width)):
+                min_tr = min(min_tr, y)
+                break
+        print("min_tr:", min_tr)
+        for _ in range(min_tr):
+            if self.height >= min_dim and self.width >= min_dim:
+                self.grid = self.grid[1:, :-1]
+                self.height -= 1
+                self.width -= 1
+    
+    def trim_bl(self,min_dim):
+        min_bl = max(self.width, self.height)
+        for x in range(self.width):
+            if any(isinstance(self.grid[y][x], Vertex) for y in range(self.height)):
+                min_bl = min(min_bl, x)
+                break
+        for y in range(self.height-1, -1, -1):
+            if any(isinstance(self.grid[y][x], Vertex) for x in range(self.width)):
+                min_bl = min(min_bl, self.height - 1 - y)
+                break
+        print("min_bl:", min_bl)
+        for _ in range(min_bl):
+            if self.height >= min_dim and self.width >= min_dim:
+                self.grid = self.grid[:-1, 1:]
+                self.height -= 1
+                self.width -= 1
+
     """Add a vertex at the specified coordinates, expanding the grid if necessary."""
-    def add_vertex(self, x, y, ghost=False):
+    def add_vertex(self, x, y, ghost=False, color=(0,0,0), width = 0):
+        expanded = False
         # if not (-1 <= x <= self.width and -1 <= y <= self.height):
         #     raise ValueError("Coordinates out of bounds")
-        while x <= -1:
-            new_col = np.full((self.height, 1), None)
-            self.grid = np.append(new_col,self.grid, axis=1)
-            self.width += 1
-            x+=1
-        while self.width <= x:
-            new_col = np.full((self.height, 1), None)
-            self.grid = np.append(self.grid,new_col, axis=1)
-            self.width += 1
-        while y <= -1:
-            new_row = np.full((1, self.width), None)
-            self.grid = np.append(new_row,self.grid, axis=0)
-            self.height += 1
+        while x <= -1 or self.width <= x or y <= -1 or self.height <= y:
+            expanded = True
+            self.expand()
             y+=1
-        while self.height <= y:
-            new_row = np.full((1, self.width), None)
-            self.grid = np.append(self.grid,new_row, axis=0)
-            self.height += 1
-        vert = Vertex(ghost=ghost)
+            x+=1
+        vert = Vertex(ghost=ghost, color=color, width=width)
         self.grid[y][x] = vert #switched x and y because of how numpy arrays are indexed
+        return expanded
     
     def add_col(self,x):
         new_col = np.full(self.height, None)
-        self.grid = np.insert(self.grid,x,new_col, axis=1)
+        self.grid = np.insert(self.grid,x+1,new_col, axis=1)
         self.width += 1
     
     def add_row(self,y):
         new_row = np.full(self.width, None)
-        self.grid = np.insert(self.grid,y,new_row, axis=0)
+        self.grid = np.insert(self.grid,y+1,new_row, axis=0)
         self.height += 1
 
     def get_vertex(self, x, y):
         if not (0 <= x < self.width and 0 <= y < self.height):
-            raise ValueError("Coordinates out of bounds")
+            return None
+            print("ValueError: Coordinates out of bounds")
         return self.grid[y][x]
     
-    def get_all_vertices(self):
+    def get_all_vertices(self): 
         vertices = []
         for row in range(self.height):
             for col in range(self.width):
@@ -96,10 +173,14 @@ class Grid:
 
 
 class Vertex:
-    def __init__(self, ghost=False, color = (0,0,0)):
+    def __init__(self, ghost=False, color = (0,0,0), width = 0):
         self.ghost = ghost
         self.color = color
+        self.width = width
+        print(self.width)
     def __print__(self):
+        return "G" if self.ghost else "V"
+    def __str__(self):
         return "G" if self.ghost else "V"
     def set_coordinates(self, x, y):
         self.x = x
